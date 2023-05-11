ebe88be22f68805ed1b30fd91b404e008cc609de
nobunobunobbu <133087229+nobunobunobbu@users.noreply.github.com>
2023-05-11 18:53:45 +0900

    Update 未来予測forTwitter.py

--- pages/未来予測forTwitter.py
+++ pages/未来予測forTwitter.py
@@ -45,10 +45,10 @@ def plot_campaign_data(filtered_df,campaign_name, target_columns,selected_column
 
     # データを日別に集計
     daily_df = campaign_df.groupby([col for col in campaign_df.columns if col != "期間"]).mean()
-    st.write(daily_df.dtypes)
+    
 
     # 日別進捗数値の欠損値を線形補間
-    daily_df = daily_df.interpolate(method='linear',limit_direction='both')
+    daily_df['target_column'] = daily_df['target_column'].interpolate(method='linear', limit_direction='both')
 
     #  reset index to make it unique
     daily_df = daily_df.reset_index()
