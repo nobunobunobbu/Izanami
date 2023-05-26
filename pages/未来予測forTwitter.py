63ea8314929b88c5fa162ef992e533b49a022dc5
nobunobunobbu <133087229+nobunobunobbu@users.noreply.github.com>
2023-05-26 15:31:13 +0900

    Update 未来予測forTwitter.py

--- pages/未来予測forTwitter.py
+++ pages/未来予測forTwitter.py
@@ -30,7 +30,7 @@ def plot_campaign_data(filtered_df,campaign_name, target_columns,selected_column
     end_date = pd.to_datetime(end_date)
     min_date = campaign_df["期間"].min()
     max_date = campaign_df["期間"].max()
-    delta = abs((min_date - end_date).days)
+    delta = abs((min_date - end_date).days)+1
 
     # 日数をインデックスに設定
     if delta < len(campaign_df):
