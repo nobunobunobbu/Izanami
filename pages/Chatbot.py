d0b3cb70591cbd25142e8d19bca659a7250f7589
nobunobunobbu <133087229+nobunobunobbu@users.noreply.github.com>
2023-07-21 19:10:08 +0900

    Create Chatbot.py

---
+++ pages/Chatbot.py
@@ -0,0 +1,31 @@
+import openai
+import streamlit as st
+from PIL import Image
+import common
+
+image = Image.open('IZANAMI.png')
+
+st.image(image,use_column_width=True)
+
+common.check_login()
+
+# ChatGPTのAPIキーを取得
+api_key = st.secrets["api_key"]["api_key"]
+st.session_state.api_key = api_key
+
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "何かお困りですか？"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+
+    openai.api_key = st.session_state.api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
