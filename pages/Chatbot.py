ffa40f5be16662d73bec01e5702cb90007f39cb6
nobunobunobbu <133087229+nobunobunobbu@users.noreply.github.com>
2023-07-26 10:40:05 +0900

    Update Chatbot.py

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -14,6 +14,7 @@ api_key = st.secrets["api_key"]["api_key"]
 st.session_state.api_key = api_key
 
 st.title("💬 Chatbot")
+model_choice = st.selectbox('モデルの選択', ['gpt-3.5-turbo', 'gpt-4'])
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "何かお困りですか？"}]
 
@@ -25,7 +26,7 @@ if prompt := st.chat_input():
     openai.api_key = st.session_state.api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = openai.ChatCompletion.create(model=model_choice, messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
