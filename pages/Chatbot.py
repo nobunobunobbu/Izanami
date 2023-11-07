1e05e13dbe9e2277a1324cc352775b0972404cc8
nobunobunobbu <133087229+nobunobunobbu@users.noreply.github.com>
2023-11-07 13:29:19 +0900

    Update Chatbot.py

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -14,7 +14,7 @@ api_key = st.secrets["api_key"]["api_key"]
 st.session_state.api_key = api_key
 
 st.title("💬 Chatbot")
-model_choice = st.selectbox('モデルの選択', ['gpt-3.5-turbo', 'gpt-4'])
+model_choice = st.selectbox('モデルの選択', ['gpt-3.5-turbo', 'gpt-4-1106-preview'])
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "何かお困りですか？"}]
 
